c67319dca720d3fe26226263cd6e9c9adb6ea52f
Debrin77 <priocas@protonmail.com>
2026-01-04 15:27:40 +0100

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,33 +1,32 @@
 import { NextRequest, NextResponse } from 'next/server';
 
 export function middleware(request: NextRequest) {
-const authHeader = request.headers.get('authorization');
+    const authHeader = request.headers.get('authorization');
 
-const USER = process.env.BASIC_AUTH_USER;
-const PASS = process.env.BASIC_AUTH_PASS;
+    const USER = process.env.BASIC_AUTH_USER;
+    const PASS = process.env.BASIC_AUTH_PASS;
 
-if (!authHeader) {
-return new NextResponse('Auth required', {
-status: 401,
-headers: {
-'WWW-Authenticate': 'Basic realm="Private App"',
-},
-});
-}
+    if (!authHeader) {
+        return new NextResponse('Auth required', {
+            status: 401,
+            headers: {
+                'WWW-Authenticate': 'Basic realm="Private App"',
+            },
+        });
+    }
 
-const encoded = authHeader.split(' ')[1];
-const decoded = Buffer.from(encoded, 'base64').toString();;
-const [user, pass] = decoded.split(':');
+    const encoded = authHeader.split(' ')[1];
+    const decoded = atob(encoded); // <-- VUELVE A DEJAR ESTA LÍNEA ASÍ (CON `atob`)
+    const [user, pass] = decoded.split(':');
 
-if (user === USER && pass === PASS) {
-return NextResponse.next();
-}
+    if (user === USER && pass === PASS) {
+        return NextResponse.next();
+    }
 
-return new NextResponse('Unauthorized', {
-status: 401,
-headers: {
-'WWW-Authenticate': 'Basic realm="Private App"',
-},
-});
-
+    return new NextResponse('Unauthorized', {
+        status: 401,
+        headers: {
+            'WWW-Authenticate': 'Basic realm="Private App"',
+        },
+    });
 }
